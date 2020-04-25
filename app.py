43361b9811f68e79b640ac931cff9abbfb059797
Cleberson Garcia <cleberson.garcia051299@gmail.com>
2020-04-25 19:57:48 -0300

    pokemons

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    pokemons = [
+        ['bulbasaur', 'https://i.imgur.com/ntcCJr4.png'],
+        ['charmander', 'https://i.imgur.com/5HmVqlW.png'],
+        ['squartle', 'https://i.imgur.com/h2aCkl3.png'],
+        ['pikachu', 'https://i.imgur.com/soCJ6J5.png']
+        ]
+    return render_template(
+        'index.html',
+        titulo = 'pokedex',
+        pokemons = pokemons
+        )
+
+if __name__ == '__main__':
+    app.run()
